0a108aa773694b4867b068820d96d716effb56d3
Ris Adams <chris@tuesdaybegins.com>
2021-11-19 12:47:01 -0500

    Updating workflow to output file as string rather than writing to a file

--- src/index.js
+++ src/index.js
@@ -1,30 +1,19 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const fetch = require('node-fetch');
-const wait = require('./wait');
-const fs = require('fs');
 
 async function run() {
   try {
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    const waitTime = core.getInput('ms');
     const repo = core.getInput('repo');
     const exclude_bots = core.getInput('exclude_bots');
-    const output = core.getInput('output');
-
-    //const ghToken = core.getInput('gh_token');
-    //const octokit = github.getOctokit(ghToken);
-
 
     const NEW_LINE = '\n';
 
     let $TEXT = '# Contributors' + NEW_LINE + NEW_LINE;
     $TEXT = $TEXT + 'Thank you to our top contributors!' + NEW_LINE + NEW_LINE;
 
-    core.info(`Waiting ${waitTime} milliseconds`);
     core.info(`Gathering info for repo: ${repo}`);
-    core.info(`Writing contributors to: ${output}`);
     core.info(`Skipping bots?: ${exclude_bots}`);
 
     const contribJsonUrl = `https://api.github.com/repos/${repo}/contributors`;
@@ -43,19 +32,10 @@ async function run() {
       $TEXT += `[${contributor.login}](${contributor.url})` + NEW_LINE;
     });
 
-    await wait(parseInt(waitTime));
     core.info($TEXT);
 
     core.info((new Date()).toTimeString());
-    core.setOutput('time', (new Date()).toTimeString());
-
-    fs.writeFile(output, $TEXT, (err) => {
-      if (err) {
-        core.setFailed(err.message);
-      }
-    });
-
-
+    core.setOutput('contribs', $TEXT);
 
   } catch (error) {
     core.setFailed(error.message);
--- src/wait.js
+++
@@ -1,8 +0,0 @@
-const wait = (ms) => new Promise((resolve) => {
-  if (typeof ms !== 'number') {
-    throw new Error('wait: ms must be a number');
-  }
-  setTimeout(() => resolve("done!"), ms);
-});
-
-module.exports = wait;
